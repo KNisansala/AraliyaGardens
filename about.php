83b055b44ad228623c074345c09450a94a65a00e
ADinushika <amalidinushika93@gmail.com>
2018-10-26 16:06:02 +0530

    #3 Create about.php Page

---
+++ about.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!--================ Basic page needs ================-->
+    <title>Milenia | About Us</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <!--================ Mobile specific metas ================-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--================ Favicon ================-->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <!--================ Google web fonts ================-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Old+Standard+TT:400,400i,700|Open+Sans:200,300,400,400i,600,600i,700,700i,800,800i|Playfair+Display:400,400i,700,700i,900,900i|Frank+Ruhl+Libre:500">
+    <!--================ Vendor CSS ================-->
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="css/linearicons.css">
+    <link rel="stylesheet" href="css/milenia-icon-font.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendors/fancybox/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="vendors/arcticmodal/jquery.arcticmodal-0.3.css">
+    <link rel="stylesheet" href="vendors/mediaelement/mediaelementplayer.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!--================ Theme CSS ================-->
+    <link rel="stylesheet" href="css/style.css">
+    <!--================ Vendor JS ================-->
+  </head>
+  <body class="milenia-body--scheme-blue milenia-body--border-layout">
+    <div class="milenia-preloader"></div>
+    <!--================ Hidden Sidebar ================--><span class="milenia-sidebar-hidden-overlay"></span>
+    <?php
+    include 'hidden-sidebar.php';
+    ?>
+    <!--================ End of Hidden Sidebar ================-->
+    <div id="milenia-page-wrapper" class="milenia-page-wrapper">
+      <!--================ Search Modal ================-->
+      <div class="milenia-d-none">
+        <div id="search-modal" class="milenia-modal milenia-modal--search">
+          <button type="button" class="milenia-icon-btn arcticmodal-close"><i class="icon icon-cross"></i></button>
+          <h3>Find Everything</h3>
+          <!--================ Search Form ================-->
+          <form role="search" method="get" class="milenia-singlefield-form milenia-form--fields-white milenia-searchform">
+            <input type="text" name="s" placeholder="Search">
+            <button type="submit"><i class="icon icon-magnifier"></i></button>
+          </form>
+          <!--================ End of Search Form ================-->
+        </div>
+      </div>
+      <!--================ End of Search Modal ================-->
+      <!--================ Header ================-->
+      <?php
+          include 'header1.php';
+      ?>
+      <!--================ End of Header ================-->
+      <div data-bg-image-src="images/breadcrumb-bg-2.jpg" class="milenia-breadcrumb text-center milenia-colorizer--scheme-dark milenia-colorizer--parallax">
+        <div class="container">
+          <h1 class="milenia-page-title">About Us</h1>
+          <nav class="milenia-breadcrumb-path">
+              <span>
+                  <a href="./">Home</a>
+              </span>/<span>About Us</span>
+          </nav>
+        </div>
+      </div>
+      <!--================ End of Breadcrumb ================-->
+      <div class="milenia-content  ">
+        <div class="container">
+          <!--================ Content Section ================-->
+          <section class="milenia-section milenia-color--black milenia-section--stretched milenia-colorizer--scheme-white">
+            <div class="row align-items-center">
+              <div class="col-lg-6">
+                <h2 class="milenia-section-title milenia-section-title--style-2">About Araliya Gardens</h2>
+                <p class="milenia-section-description milenia-section-description--style-2">Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis.</p>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris ferme ntum dictum magna. Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. </p>
+                <p>Integer rutrum ante ue lacus. Vestibulum libero nisl, porta vel, scelerisque eget, malesuada at, neque. Vivamus eget nibh. Etiam cursus leo vel metus. Nulla facilisi. Aenean nec eros. Vestibulum ante ipsum primis in faucibus.</p>
+              </div>
+              <div class="col-lg-6"><img src="images/about-image-1.jpg" alt=""></div>
+            </div>
+          </section>
+          <!--================ End of Content Section ================-->
+          <!--================ Content Section ================-->
+<!--          <div class="milenia-section milenia-section--stretched-content-no-px milenia-section--no-py">
+            <div class="owl-carousel owl-carousel--nav-edges owl-carousel--nav-huge owl-carousel--nav-inside owl-carousel--nav-hover-white milenia-simple-slideshow"><img src="images/slide-18.jpg" alt=""><img src="images/slide-19.jpg" alt=""><img src="images/slide-20.jpg" alt=""></div>
+          </div>-->
+          <!--================ End of Content Section ================-->
+
+          <!--================ Content Section ================-->
+<!--          <div class="milenia-section milenia-section--no-py milenia-section--stretched-content-no-px">
+            ================ Gallery ================
+            <div class="milenia-instafeed">
+              <div id="instafeed" data-get="user" data-user="customer" data-limit="6" class="milenia-grid milenia-grid--no-gutters milenia-grid--cols-6"></div>
+            </div>
+            ================ End of Gallery ================
+          </div>-->
+          <!--================ End of Content Section ================-->
+        </div>
+      </div>
+      <!--================ Footer ================-->
+      <?php
+      include 'footer.php';
+      ?>
+      <!--================ End of Footer ================-->
+    </div>
+    <script src="vendors/modernizr.js"></script>
+    <script src="vendors/jquery-3.3.1.min.js"></script>
+    <script src="vendors/nicescroll/jquery.nicescroll.min.js"></script>
+    <script src="vendors/jquery.easing.1.3.min.js"></script>
+    <script src="vendors/instafeed.min.js"></script>
+    <script src="vendors/instafeed.wrapper.min.js"></script>
+    <script src="vendors/jquery.parallax-1.1.3.min.js"></script>
+    <script src="vendors/monkeysan.sameheight.min.js"></script>
+    <script src="vendors/isotope.pkgd.min.js"></script>
+    <script src="vendors/monkeysan.jquery.nav.1.0.min.js"></script>
+    <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+    <script src="vendors/arcticmodal/jquery.arcticmodal-0.3.min.js"></script>
+    <script src="vendors/fancybox/jquery.fancybox.min.js"></script>
+    <script src="vendors/monkeysan.validator.min.js"></script>
+    <script src="vendors/handlebars-v4.0.5.min.js"></script>
+    <script src="vendors/mad.customselect.js"></script>
+    <script src="vendors/retina.min.js"></script>
+    <script src="js/modules/milenia.sidebar-hidden.min.js"></script>
+    <script src="js/modules/milenia.newsletter-form.min.js"></script>
+    <script src="js/modules/milenia.alert-box.min.js"></script>
+    <script src="js/modules/milenia.isotope.js"></script>
+    <script src="js/modules/milenia.tabbed-grid.min.js"></script>
+    <script src="js/modules/milenia.sticky-header-section.min.js"></script>
+    <script src="js/milenia.app.js"></script>
+  </body>
+</html>
--- footer.php
+++ footer.php
@@ -1,143 +1,62 @@
 <footer id="milenia-footer" class="milenia-footer">
-                <!--================ Footer row ================-->
-                <div class="milenia-footer-row milenia-footer-row--widget-border">
-                    <div class="container">
-                        <div class="milenia-footer-row--inner milenia-footer-row-inner--no-offsets">
-                            <div class="milenia-grid milenia-grid--valign-middle milenia-grid--cols-3">
-                                <!--================ Widget ================-->
-                                <section class="milenia-grid-item milenia-widget">
-                                    <div class="milenia-grid-item-inner">
-                                        <div class="milenia-grid-item-content">
-                                            <h5 class="milenia-widget-title">Current Weather</h5>
-                                            <p>Rome, Italy</p>
-                                            <div class="milenia-weather-indicator milenia-weather-indicator--style-3"><i class="icon icon-sun"></i>15<sup>°C<span>/°F</span></sup></div>
-                                        </div>
-                                    </div>
-                                </section>
-                                <!--================ End of Widget ================-->
-                                <!--================ Widget ================-->
-                                <section class="milenia-grid-item milenia-widget milenia-widget--center-aligned">
-                                    <div class="milenia-grid-item-inner">
-                                        <div class="milenia-grid-item-content">
-                                            <h5 class="milenia-widget-title">Stay Connected</h5>
-                                            <p>Follow us on social media channels</p>
-                                            <ul class="milenia-social-icons milenia-list--unstyled">
-                                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-tripadvisor"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </section>
-                                <!--================ End of Widget ================-->
-                                <!--================ Widget ================-->
-                                <section class="milenia-grid-item milenia-widget milenia-widget--newsletter">
-                                    <div class="milenia-grid-item-inner">
-                                        <div class="milenia-grid-item-content">
-                                            <h5 class="milenia-widget-title">Newsletter Sign Up</h5>
-                                            <p>Sign up for news and special offers</p>
-                                            <form class="milenia-singlefield-form milenia-newsletter-form">
-                                                <input type="email" name="email" placeholder="Enter Your Email Address">
-                                                <button type="submit"><i class="icon icon-envelope"></i></button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </section>
-                                <!--================ End of Widget ================-->
-                            </div>
+        <!--================ Footer row ================-->
+        <div class="milenia-footer-row milenia-colorizer--scheme-dark milenia-footer-row--medium">
+          <div class="container">
+            <div class="milenia-footer-row--inner">
+              <div class="milenia-grid milenia-grid--cols-1">
+                <!--================ Widget ================-->
+                <div class="milenia-grid-item milenia-widget milenia-widget--info">
+                  <div class="milenia-grid-item-inner">
+                    <div class="milenia-grid-item-content">
+                      <div class="milenia-info-widget">
+                        <div class="milenia-info-widget-logo"><a href="index.html" class="milenia-ln--independent"><img src="images/logo-white.png" alt=""></a></div>
+                        <div class="milenia-grid milenia-grid--cols-3">
+                          <div class="milenia-grid-item">
+                            <address><span class="milenia-text-color--contrast">Address: </span>9863 - 9867 Mill Road, <br>Cambridge, MG09 99HT </address><a href="#" class="milenia-uppercased-link">Get Direction</a>
+                          </div>
+                          <div class="milenia-grid-item">
+                            <address><span class="milenia-text-color--contrast">Phone: </span>+1 800 603 6035<br><span class="milenia-text-color--contrast">Fax: </span>+1 800 603 6038</address>
+                          </div>
+                          <div class="milenia-grid-item milenia-widget--email">
+                            <address><span class="milenia-text-color--contrast">Email: </span><a href="mailto:#">mail@companyname.com</a></address><a href="#" class="milenia-btn milenia-btn--unbordered">Submit Request</a>
+                          </div>
                         </div>
+                      </div>
                     </div>
+                  </div>
                 </div>
-                <!--================ End of Footer row ================-->
-                <!--================ Footer row ================-->
-                <div class="milenia-footer-row milenia-colorizer--scheme-dark milenia-footer-row--uppercased-titles milenia-footer-row--medium">
-                    <div class="container">
-                        <div class="milenia-footer-row--inner milenia-footer-row-inner--no-offsets">
-                            <div class="milenia-grid milenia-grid--cols-4">
-                                <!--================ Widget ================-->
-                                <section class="milenia-grid-item milenia-widget milenia-widget--left-aligned">
-                                    <div class="milenia-grid-item-inner">
-                                        <div class="milenia-grid-item-content">
-                                            <h5 class="milenia-widget-title">Contact</h5>
-                                            <ul class="milenia-details-list milenia-list--unstyled">
-                                                <li><span class="milenia-text-color--contrast">Address:&nbsp;</span>9863 - 9867 Mill Road,<br>Cambridge, MG09 99HT</li>
-                                                <li><span class="milenia-text-color--contrast">Phone:&nbsp;</span>+1 800 603 6035</li>
-                                                <li><span class="milenia-text-color--contrast">Fax:&nbsp;</span>+1 800 889 9898</li>
-                                                <li><span class="milenia-text-color--contrast">Email:&nbsp;</span><a href="mailto:#">mail@companyname.com</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </section>
-                                <!--================ End of Widget ================-->
-                                <!--================ Widget ================-->
-                                <div class="milenia-grid-item milenia-widget milenia-widget--left-aligned">
-                                    <div class="milenia-grid-item-inner">
-                                        <div class="milenia-grid-item-content">
-                                            <h5 class="milenia-widget-title">Main Menu</h5>
-                                            <nav>
-                                                <ul>
-                                                    <li><a href="#">About</a></li>
-                                                    <li><a href="#">Special Offers</a></li>
-                                                    <li><a href="#">News</a></li>
-                                                    <li><a href="#">Contact Us</a></li>
-                                                </ul>
-                                            </nav>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--================ End of Widget ================-->
-                                <!--================ Widget ================-->
-                                <div class="milenia-grid-item milenia-widget milenia-widget--left-aligned">
-                                    <div class="milenia-grid-item-inner">
-                                        <div class="milenia-grid-item-content">
-                                            <h5 class="milenia-widget-title">Things To Do</h5>
-                                            <nav>
-                                                <ul>
-                                                    <li><a href="#">Wellness &amp; Spa</a></li>
-                                                    <li><a href="#">Restaurants &amp; Bars</a></li>
-                                                    <li><a href="#">Excursions</a></li>
-                                                    <li><a href="#">Events</a></li>
-                                                </ul>
-                                            </nav>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--================ End of Widget ================-->
-                                <!--================ Widget ================-->
-                                <div class="milenia-grid-item milenia-widget milenia-widget--left-aligned">
-                                    <div class="milenia-grid-item-inner">
-                                        <div class="milenia-grid-item-content">
-                                            <h5 class="milenia-widget-title">Rooms &amp; Suits</h5>
-                                            <nav>
-                                                <ul>
-                                                    <li><a href="#">Classic</a></li>
-                                                    <li><a href="#">Superior</a></li>
-                                                    <li><a href="#">Deluxe</a></li>
-                                                    <li><a href="#">Master</a></li>
-                                                </ul>
-                                            </nav>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--================ End of Widget ================-->
-                            </div>
-                        </div>
+                <!--================ End of Widget ================-->
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--================ End of Footer row ================-->
+        <!--================ Footer row ================-->
+        <div class="milenia-footer-row milenia-colorizer--scheme-dark milenia-footer-row--thin">
+          <div class="container">
+            <div class="milenia-footer-row--inner">
+              <div class="milenia-grid milenia-grid--valign-middle milenia-grid--cols-1">
+                <!--================ Widget ================-->
+                <div class="milenia-grid-item milenia-widget milenia-widget--list-horizontal">
+                  <div class="milenia-grid-item-inner">
+                    <div class="milenia-grid-item-content">
+                      <nav class="milenia-tt-uppercase">
+                        <ul>
+                          <li><a href="#">About</a></li>
+                          <li><a href="#">Special Offers</a></li>
+                          <li><a href="#">News</a></li>
+                          <li><a href="#">Events</a></li>
+                          <li><a href="#">Contact Us</a></li>
+                        </ul>
+                      </nav>
                     </div>
+                  </div>
                 </div>
-                <!--================ End of Footer row ================-->
-                <!--================ Footer row ================-->
-                <div class="milenia-footer-row milenia-colorizer--scheme-dark milenia-footer-row--thin">
-                    <div class="container">
-                        <div class="milenia-footer-row--inner milenia-footer-row-inner--no-offsets">
-                            <div class="milenia-grid milenia-grid--cols-1">
-                                <div class="milenia-grid-item milenia-widget milenia-widget--center-aligned">Copyright © 2018 Milenia Luxury Hotel. All Rights Reserved.</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--================ End of Footer row ================-->
-            </footer>
+                <!--================ End of Widget ================-->
+                <div class="milenia-grid-item milenia-widget">Copyright © 2018 Milenia Beach Resort. All Rights Reserved.</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--================ End of Footer row ================-->
+      </footer>
