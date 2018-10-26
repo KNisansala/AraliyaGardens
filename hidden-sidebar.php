71ccb54519434edbce671f2b668e11ae11d2a679
ADinushika <amalidinushika93@gmail.com>
2018-10-26 13:35:24 +0530

    #1 Create index.php Page

---
+++ hidden-sidebar.php
@@ -0,0 +1,70 @@
+<aside id="milenia-sidebar-hidden" aria-hidden="true" class="milenia-sidebar-hidden">
+    <button type="button" data-sidebar-hidden="#milenia-sidebar-hidden" aria-expanded="false" aria-controls="milenia-sidebar-hidden" aria-haspopup="true" class="milenia-sidebar-hidden-close">
+        <i class="icon icon-cross"></i>
+    </button>
+    <!--================ Hidden Sidebar Header ================-->
+    <header class="milenia-sidebar-hidden-header">
+        <div class="milenia-sidebar-hidden-items">
+            <div><a href="./" class="milenia-ln--independent"><img src="images/logo-lightbrown.png" alt=""></a></div>
+        </div>
+    </header>
+    <!--================ End of Hidden Sidebar Header ================-->
+    <!--================ Hidden Sidebar Content ================-->
+    <div class="milenia-sidebar-hidden-content">
+        <!--================ Navigation ================-->
+        <nav class="milenia-navigation-container milenia--nice-scrolled">
+            <ul class="milenia-navigation-vertical">
+                <li class="menu-item menu-item-has-children current-menu-item">
+                    <a href="index.php" class="milenia-ln--independent">Home</a>
+                </li>
+                <li class="menu-item menu-item-has-children">
+                    <a href="about.php" class="milenia-ln--independent">About Us</a>
+                </li>
+                <li class="menu-item menu-item-has-children">
+                    <a href="facilities.php" class="milenia-ln--independent">Facilities</a>
+                </li>
+                <li class="menu-item menu-item-has-children">
+                    <a href="rooms.php" class="milenia-ln--independent">Rooms</a>
+                </li>
+                <li class="menu-item menu-item-has-children">
+                    <a href="things-to-do.php" class="milenia-ln--independent">Thing TO Do</a>
+                </li>
+                <li class="menu-item menu-item-has-children">
+                    <a href="gallery.php" class="milenia-ln--independent">Gallery</a>
+                </li>
+                <li class="menu-item menu-item-has-children">
+                    <a href="guest-feedback.php" class="milenia-ln--independent">Guest Feedback</a>
+                </li>
+                <li class="menu-item menu-item-has-children mega-menu">
+                    <a href="booking.php" class="milenia-ln--independent">Booking</a>
+                </li>
+                <li class="menu-item menu-item-has-children">
+                    <a href="contacts.php" class="milenia-ln--independent">Contact Us</a>
+                </li>
+            </ul>
+        </nav>
+        <!--================ End of Navigation ================-->
+    </div>
+    <!--================ End of Hidden Sidebar Content ================-->
+
+    <!--================ Hidden Sidebar Footer ================-->
+    <footer class="milenia-sidebar-hidden-footer">
+        <div class="milenia-sidebar-hidden-items">
+            <div>
+                <ul class="milenia-sidebar-hidden-actions milenia-list--hr milenia-list--unstyled">
+                    <li><a href="#" data-arctic-modal="#search-modal" class="milenia-ln--independent">Search</a></li>
+                    <li><a href="#" class="milenia-ln--independent">Webcams</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul class="milenia-social-icons milenia-social-icon--scheme-secondary milenia-list--unstyled">
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-tripadvisor"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <!--================ End of Hidden Sidebar Footer ================-->
+</aside>
