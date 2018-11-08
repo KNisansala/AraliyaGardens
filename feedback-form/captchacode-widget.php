1eba2b2036529c72e90975bc85b8d29d4c080ea9
ADinushika <amalidinushika93@gmail.com>
2018-11-08 08:58:23 +0530

    #43 Make Responsive

---
+++ feedback-form/captchacode-widget.php
@@ -0,0 +1,24 @@
+<?php
+$captchacode = rand();
+
+?>               
+
+<img src="feedback-form/Generate_Captcha_Code.php?rand=<?php echo $captchacode; ?>" id='captchaimg'  style=" margin-top: 35px; margin-left: -10px;">  
+
+<a href='javascript: refreshCaptcha();' class="contact-detail">
+    <div class="refreshbox">
+        <div class="refresh-img">        
+            <img style="border:none;" src="feedback-form/img/refresh.png" title="Click to change the code"/>
+        </div>
+    </div>
+</a>
+
+<script language='JavaScript' type='text/javascript'>
+
+    function refreshCaptcha() {
+        var img = document.images['captchaimg'];
+        var c = Math.round(Math.random() * 10000);
+        img.src = img.src.substring(0, img.src.lastIndexOf("?")) + "?rand=" + c;
+    }
+
+</script>
